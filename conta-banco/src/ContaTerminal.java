e3d9ab9377723c9a5cde14a97f6d06190c45521a
MarciaAguiar1701 <marciaatoa@gmail.com>
2024-05-23 17:24:49 -0300

    Desafio-trilha-java-basico

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,41 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args){
+
+		
+        public static (ContaTerminal){
+			
+		int numero = (1021);
+		String agencia = (067-8);
+		String nomeCliente = ("Márcia Aguiar");
+		double saldo (234.33);
+	
+	}
+
+	Scanner scanner = new Scanner(System.in).useLocale(Locale.US)
+
+
+		System.out.println("Por favor, digite o número da Agência !");
+		int numero = scanner.next();
+
+		System.out.println("Por favor, digite sua Agencia");
+		String agencia = scanner.next();
+
+		System.out.println("Por favor, digite seu nome");
+		String nomeCliente = scanner.next();
+		System.out.println("Seu saldo é R$ 234,33");
+		double saldo = scanner.next();
+
+		System.out.println("Obrigada por criar uma conta em nosso, sua agencia[agencia] sua conta[numero] e seu saldo[saldo] ja esta disponivel")
+
+
+
+    }
+
+		  
+
+
     }
 }
